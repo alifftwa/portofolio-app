aef5dbcbf8844557a7bae51514b8f81e6a377814
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-10-06 10:48:30 +0700

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-center items-center w-[2000px] h-16 bg-[#1C2336]">
+      <div className="flex justify-center items-center w-full h-16 bg-[#1C2336]">
         <p className="text-white">© 2023 Alifftwa. All rights reserved.</p>
       </div>
     </>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,18 +7,18 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="h-[636px] bg-[#0B1121] auto-rows-auto">
-        <div className="flex h-96 w-auto bg-[#0B1121]">
-          {/* Kiri */}
-          <div className="pl-96 pr-52 mt-52">
+      <div className="max-h-max bg-[#0B1121] auto-rows-auto">
+        <div className="flex h-full w-full bg-[#0B1121]">
+          {/* Left */}
+          <div className="pl-96 pr-52 mt-72">
             <div className="flex items-center">
-              <p className="text-[#DBFF00]">Hello World</p>
+              <p className="text-[#DBFF00] text-[30px]">Hello World</p>
               <div className="w-10 h-10 mx-2">
                 <img src={wavhand} alt="" />
               </div>
             </div>
-            <div className="text-white">
-              <h1>Alif Fatwa Ramadhani</h1>
+            <div className="text-white text-[37px]">
+              <h1>I'm Alif Fatwa Ramadhani</h1>
             </div>
             <div className="">
               <p className="text-white font-thin">
@@ -30,10 +30,10 @@ const Home = () => {
             </div>
           </div>
 
-          {/* Kanan */}
-          <div className="">
+          {/* Right */}
+          <div className="h-full">
             <div className="pr-80">
-              <div className="pt-10 max-w-max">
+              <div className="pt-[166px] max-w-max">
                 <img src={self} alt="" />
               </div>
             </div>
